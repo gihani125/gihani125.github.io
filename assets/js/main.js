b3ce98c7e11d31310bc79e3e0aaefe28311a61dd
SahanMonaara <sahanmonaara2015@gmail.com>
2023-03-05 17:48:34 +0000

    new changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -175,7 +175,7 @@
     let portfolioContainer = select('.portfolio-container');
     if (portfolioContainer) {
       let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item'
+          itemSelector: '.portfolio-item', filter: '.filter-swot'
       });
 
       let portfolioFilters = select('#portfolio-flters li', true);
